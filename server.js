b8e28bf4e68e146bb6da9299f857d2908013a2dc
kanishka <kanishkamaingi21@gmail.com>
2024-09-30 20:56:29 -0400

    acedemic integrity

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+/********************************************************************************
+* WEB322 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*
+* Name: -KANISHKA Student ID: 155238223 Date: 30-09-2024
+*
+* Published URL: https://assignment-2-eosin-phi.vercel.app/
+*
+********************************************************************************/
 const legoData = require("./modules/legoSets");
 legoData.initialize();
 const express = require('express'); 
 const app = express(); 
-const HTTP_PORT = process.env.PORT || 8080; 
+const HTTP_PORT = 8080; 
 
 app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
 
